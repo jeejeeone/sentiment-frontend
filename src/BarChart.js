6c93cdf3b5c0fcb8a685d75f4cf08f9e8147ed5a
Jari Jokisalo <jj@Jaris-MBP.wlan>
2021-07-23 14:21:55 +0300

    Add scss, initial implementation for bar chart

---
+++ src/BarChart.js
@@ -0,0 +1,73 @@
+const barConfig = {
+    height: 25,
+    padding: 3,
+    barPadding: 5,
+    barMinWidthPercentage: 0.3,
+    // Approximation
+    charWidth: 10,
+    markerLength: 5
+}
+
+const Bar = (props) => {
+    const barSize = barConfig.height + barConfig.padding;
+
+    const textY = ((props.index + 1) * barSize) / 2 + (props.index * barSize) / 2;
+    const markerY = textY - 1;
+
+    const percentage = (percentage) => percentage + "%"
+    const barY = props.index * barSize;
+    const barXWithPadding = props.maxTickerCharacters * barConfig.charWidth + barConfig.barPadding
+    const barWidth =
+        percentage(
+        props.widthPercentage < barConfig.barMinWidthPercentage ?
+            barConfig.barMinWidthPercentage : props.widthPercentage
+        )
+
+    const countY = textY
+    const countX = barXWithPadding + 5
+
+    return (
+        <g className="barItem">
+            <text className="ticker" y={textY} dy=".35em">{props.text}</text>
+            <line x1={barXWithPadding - barConfig.markerLength} y1={markerY} x2={barXWithPadding} y2={markerY}></line>
+            <g>
+                <rect
+                    className="bar"
+                    width={barWidth}
+                    height={barConfig.height}
+                    y={barY}
+                    x={barXWithPadding}/>
+                <text className="count" y={countY} x={countX} dy=".35em">{props.count}</text>
+            </g>
+        </g>
+    );
+}
+
+const BarChart = (props) => {
+    const maxTickerCharacters = Math.max(...props.tickers.map(item => item.ticker.length));
+    const maxCount = Math.max(...props.tickers.map(item => item.count));
+
+    return (
+        <div>
+            <svg width="100%" height={props.tickers.length * (barConfig.height + barConfig.padding)}>
+                {
+                    props.tickers.map((item,index) => {
+                        const width = (item.count / maxCount * 100);
+
+                        return <Bar
+                            key={item.ticker}
+                            widthPercentage={width}
+                            text={item.ticker}
+                            count={item.count}
+                            index={index}
+                            maxTickerCharacters={maxTickerCharacters}
+                        />
+                    })
+                }
+            </svg>
+        </div>
+    );
+}
+
+
+export default BarChart
--- src/index.js
+++ src/index.js
@@ -1,70 +1,31 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import { render } from "react-dom";
-import { ResponsiveBar } from "@nivo/bar";
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import FilterListIcon from '@material-ui/icons/FilterList';
 import Collapse from '@material-ui/core/Collapse';
+import axios from 'axios';
+import BarChart from "./BarChart";
+import './index.scss';
 
-const options = ['Option 1', 'Option 2'];
 const timeFilterValues = ['24 hours', '7 days', '30 days', '100 days'];
-
-const data = [
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "CLNE",
-        "count": 89
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "WKHS",
-        "count": 93
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "CLOV",
-        "count": 103
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "AMC",
-        "count": 110
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "SPY",
-        "count": 140
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "GME",
-        "count": 167
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "WISH",
-        "count": 175
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "AMD",
-        "count": 223
-    },
-    {
-        "one_day": "2021-07-01T00:00:00",
-        "mention": "MU",
-        "count": 356
-    }
-];
 
 const App = () => {
     const [timeFilter, setTimeFilter] = React.useState(timeFilterValues[0]);
     const [showFilters, setShowFilters] = React.useState(false);
     const [filtersLabel, setFiltersLabel] = React.useState("Show Filters");
+    const [mentions, setMentions] = React.useState([])
+
+    useEffect(() => {
+        axios.get(`/api/ui/mentions/last/` + timeFilter).then(
+            res => {
+                setMentions(res.data.mentions)
+            }
+        )
+    }, [timeFilter]);
 
     const toggleShowFilter = () => {
         setShowFilters(!showFilters)
@@ -91,58 +52,41 @@ const App = () => {
                         />
                     </Grid>
                     <Grid item xs={7}/>
-                    <Grid item xs={3} container justify="flex-end">
+                    <Grid item xs={3} container justify="flexend">
                         <Button
                             endIcon={<FilterListIcon>{filtersLabel}</FilterListIcon>}
                             onClick={toggleShowFilter}
                         >{filtersLabel}</Button>
                     </Grid>
                 </Grid>
-            <Collapse in={showFilters} style={{paddingTop: '12px'}}>
+            {/* TODO: Remove collapse not needed */}
+            { showFilters ?
+                <Collapse in={showFilters} style={{paddingTop: '12px', paddingBottom: '12px'}}>
                     <Grid container spacing={3}>
-                    <Grid item xs={3}>
-                        <Autocomplete
-                            id="tickers-filter"
-                            renderInput={(params) => <TextField {...params} label="Ticker" variant="outlined" size="small" />}
-                        />
+                        <Grid item xs={3}>
+                            <Autocomplete
+                                id="tickers-filter"
+                                renderInput={(params) => <TextField {...params} label="Ticker" variant="outlined" size="small" />}
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Autocomplete
+                                id="sector-filter"
+                                renderInput={(params) => <TextField {...params} label="Sector" variant="outlined" size="small" />}
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            <Autocomplete
+                                id="industry-filter"
+                                renderInput={(params) => <TextField {...params} label="Industry" variant="outlined" size="small" />}
+                            />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                        <Autocomplete
-                            id="sector-filter"
-                            renderInput={(params) => <TextField {...params} label="Sector" variant="outlined" size="small" />}
-                        />
-                    </Grid>
-                    <Grid item xs={3}>
-                        <Autocomplete
-                            id="industry-filter"
-                            renderInput={(params) => <TextField {...params} label="Industry" variant="outlined" size="small" />}
-                        />
-                    </Grid>
-                    </Grid>
-            </Collapse>
+                </Collapse> : null
+            }
             <Grid container spacing={3}>
                 <Grid item xs={12}>
-                    <div style={{ height: "700px"}}>
-                        <ResponsiveBar
-                            data={data}
-                            keys={["count"]}
-                            indexBy="mention"
-                            layout="horizontal"
-                            axisBottom={null}
-                            margin={{
-                                left: 45
-                            }}
-                            axisLeft={{
-                                tickSize: 5,
-                                tickPadding: 5,
-                                tickRotation: 0,
-                                legend: "",
-                                legendPosition: 'middle',
-                                legendOffset: -40
-                            }}
-                            enableGridY={null}
-                        />
-                    </div>
+                    <BarChart tickers={mentions}/>
                 </Grid>
             </Grid>
         </Container>
