16c30d9a17e72f73ec751264914db798cf7c620b
Jari Jokisalo <jj@Jaris-MBP.wlan>
2021-08-02 22:51:56 +0300

    PoC line graph

--- src/ticker/TickerView.js
+++ src/ticker/TickerView.js
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import Container from '@material-ui/core/Container';
+import React, { useEffect, useState } from "react";
+import Container from "@material-ui/core/Container";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { ResponsiveLine } from '@nivo/line'
+import { ResponsiveLine } from "@nivo/line";
 import { useRecoilState } from "recoil";
-import { breadcrumbsState, root } from "../common/SentimentBreadcrumbs"
+import { breadcrumbsState, root } from "../common/SentimentBreadcrumbs";
 
 const data1 = [
     {
@@ -12,21 +12,21 @@ const data1 = [
         data: [
             { y: "123", x: "2019-05-29" },
             { y: "123", x: "2019-05-30" },
-            { y: "12", x: "2019-05-31" }
-        ]
-    }
+            { y: "12", x: "2019-05-31" },
+        ],
+    },
 ];
 
 const TickerView = () => {
-    const [data, setData] = React.useState([])
+    const [data, setData] = React.useState([]);
 
-    const [breadcrumbs, setBreadcrumbs] = useRecoilState(breadcrumbsState)
+    const [breadcrumbs, setBreadcrumbs] = useRecoilState(breadcrumbsState);
 
     let params = useParams();
 
     useEffect(() => {
         axios.get(`/api/ui/mentions/ticker/` + params.ticker).then(
-            res => {
+            (res) => {
                 const data = [{
                     id: res.data.ticker,
                     data: res.data.tickerMentions.map(item => {
@@ -35,16 +35,16 @@ const TickerView = () => {
                             x: item.date + " 00:00"
                         }
                     })
-                }]
+                }];
 
-                console.log(data)
+                console.log(data);
             })
 
         setBreadcrumbs([
             root,
             {
                 label: params.ticker,
-                path: "/ticker/" + params.ticker
+                path: "/ticker/" + params.ticker,
             }
         ]);
     }, []);
@@ -56,29 +56,29 @@ const TickerView = () => {
 
                 <ResponsiveLine
                     data={data1}
-                    margin={{ top: 40, right: 150, bottom: 50, left: 40 }}
-                    yScale={{ type: "point" }}
+                    margin={{ top: 40, right: 150, bottom: 50, left: 40, }}
+                    yScale={{ type: "point", }}
                     xScale={{
                         type: "time",
                         format: "%Y-%m-%d",
-                        precision: "day"
+                        precision: "day",
                     }}
                     xFormat="time:%Y-%m-%d"
                     axisBottom={{
                         tickRotation: 44,
                         format: "%Y-%m-%d",
                         legendPosition: "middle",
-                        tickValues: 'every day',
+                        tickValues: "every day",
                     }}
                     pointSize={10}
                     pointColor="white"
                     pointBorderWidth={2}
-                    pointBorderColor={{ from: "serieColor" }}
+                    pointBorderColor={{ from: "serieColor", }}
                     useMesh={true}
                 />
             </div>
         </Container>
-    )
+    );
 }
 
 export default TickerView;
